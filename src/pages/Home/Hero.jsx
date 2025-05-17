4bec27e89d0bd50d5eb32f5c2be5e13464de2224
EmranEmon47 <aa.emran47@gmail.com>
2025-05-17 12:19:23 +0600

    nav

---
+++ src/pages/Home/Hero.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hero = () => {
+  return <div>I am Hero</div>;
+};
+
+export default Hero;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../../Components/Shared/Nav.jsx";
+import Hero from "./Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
