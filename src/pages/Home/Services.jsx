ffb42b3f7af21b7ca1daed03c40a4db199cfbeaa
EmranEmon47 <aa.emran47@gmail.com>
2025-05-28 23:12:53 +0600

    services text added

--- src/pages/Home/Services.jsx
+++ src/pages/Home/Services.jsx
@@ -3,25 +3,25 @@ import React from "react";
 const Services = () => {
   return (
     <div className="w-full py-12 max-w-[calc(100%-440px)] mx-auto flex flex-col md:flex-row gap-4 p-4">
-      <div>
-        <h3>Free Shipping on all ordres</h3>
-        <p>
+      <div className=" flex flex-col justify-center items-center  gap-2">
+        <h3 className="text-lg font-semibold">Free Shipping on all orders</h3>
+        <p className="text-sm font-normal">
           Get Free Shipping on all orders over $75 and free returns to our UK
           returns centre! Items are dispatched from the US and will arrive in
           5-8 days.
         </p>
       </div>
-      <div>
-        <h3>Amazing customer service</h3>
-        <p>
+      <div className=" flex flex-col justify-center items-center  gap-2">
+        <h3 className="text-lg font-semibold">Amazing customer service</h3>
+        <p className="text-sm font-normal">
           Get Free Shipping on all orders over $75 and free returns to our UK
           returns centre! Items are dispatched from the US and will arrive in
           5-8 days.
         </p>
       </div>
-      <div>
-        <h3>No Customs or Duty Fees!</h3>
-        <p>
+      <div className=" flex flex-col justify-center items-center  gap-2">
+        <h3 className="text-lg font-semibold">No Customs or Duty Fees!</h3>
+        <p className="text-sm font-normal">
           We pay these fees so you don’t have to! The total billed at checkout
           is the final amount you pay, inclusive of VAT, with no additional
           charges at the time of delivery!
