8531f61642b0983490c63fb57f3cbb8979ac1f9b
EmranEmon47 <aa.emran47@gmail.com>
2025-06-11 21:21:04 +0600

    envvionment variables file created

--- src/firebase.js
+++ src/firebase.js
@@ -3,23 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-
-    apiKey: "AIzaSyBgUu36Nv7go_RuL5SUayz-slKlLXHoDUo",
-
-    authDomain: "flatsome-ecommerce.firebaseapp.com",
-
-    projectId: "flatsome-ecommerce",
-
-    storageBucket: "flatsome-ecommerce.firebasestorage.app",
-
-    messagingSenderId: "114143823717",
-
-    appId: "1:114143823717:web:8722a04737023fa750838c",
-
-    measurementId: "G-8GWQH99671"
-
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
