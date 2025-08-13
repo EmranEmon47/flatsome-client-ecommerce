f05a089b5a2164aed4bec374e1a5bd7e6fdbb13f
EmranEmon47 <aa.emran47@gmail.com>
2025-08-14 02:44:20 +0600

    modify and add link in the footer

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full mx-auto">
-      <footer className="flex justify-center p-4 text-black transition-all duration-500 backdrop-blur-lg bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 hover:border-white/30 dark:text-white item-center text-base-content">
+    <div className="w-full mx-auto mt-2 lg:mt-12">
+      <footer className="flex justify-center p-4 py-0 mx-auto text-black transition-all duration-500 lg:py-8 backdrop-blur-lg bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 hover:border-white/30 dark:text-white item-center text-base-content">
         <div>
-          <p>
+          <p className="text-sm text-center">
             Copyright © {new Date().getFullYear()} - All right reserved by
-            Flatsome Industries Ltd
+            Flatsome Industries Ltd{" "}
+            <a
+              href="https://github.com/EmranEmon47"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              (By Abdullah Al Emran Emon)
+            </a>
           </p>
         </div>
       </footer>
